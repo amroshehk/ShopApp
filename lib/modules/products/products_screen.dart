09587e9479e76e364356591cd8b903f1fbb3843e
Amro Shekh Zain <amroshehzen@hotmail.com>
2024-06-06 04:13:12 +0300

    Add product builder item

--- lib/modules/products/products_screen.dart
+++ lib/modules/products/products_screen.dart
@@ -2,11 +2,13 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/models/home/home_model.dart';
 
 import '../../layouts/shop/cubit.dart';
 import '../../layouts/shop/states.dart';
+import '../../shared/styles/colors.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({super.key});
@@ -19,8 +21,7 @@ class ProductsScreen extends StatelessWidget {
           ShopCubit cubit = ShopCubit.get(context);
           return ConditionalBuilder(
             condition: cubit.homeModel != null,
-            builder: (context) =>
-                productBuilder(cubit.homeModel?.data?.banners),
+            builder: (context) => productBuilder(cubit.homeModel!, context),
             fallback: (context) =>
                 const Center(child: CircularProgressIndicator()),
           );
@@ -30,32 +31,137 @@ class ProductsScreen extends StatelessWidget {
     );
   }
 
-  Widget productBuilder(List<BannerModel>? banners) {
-    return Column(
-      children: [
-        CarouselSlider(
-          options: CarouselOptions(
-            height: 250.0,
-            initialPage: 0,
-            enableInfiniteScroll: true,
-            autoPlay: true,
-            viewportFraction: 1.0,
-            autoPlayInterval: Duration(seconds: 3),
-            autoPlayAnimationDuration: Duration(seconds: 1),
-            autoPlayCurve: Curves.fastOutSlowIn,
-            enlargeCenterPage: true,
-            enlargeFactor: 0.3,
-            scrollDirection: Axis.horizontal,
+  Widget productBuilder(HomeModel homeModel, BuildContext context) {
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 250.0,
+              initialPage: 0,
+              enableInfiniteScroll: true,
+              autoPlay: true,
+              viewportFraction: 1.0,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(seconds: 1),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              enlargeFactor: 0.3,
+              scrollDirection: Axis.horizontal,
+            ),
+            items: homeModel.data?.banners.map((banner) {
+              return Image(
+                image: NetworkImage('${banner.image}'),
+                width: double.infinity,
+                fit: BoxFit.cover,
+              );
+            }).toList(),
           ),
-          items: banners?.map((banner) {
-            return Image(
-              image: NetworkImage('${banner.image}'),
-              width: double.infinity,
-              fit: BoxFit.cover,
-            );
-          }).toList(),
-        )
-      ],
+          SizedBox(height: 10.0),
+          Container(
+              color: Colors.grey[300],
+              child: productBuilderGrid(homeModel.data!.products, context))
+        ],
+      ),
     );
   }
+
+  Widget productBuilderGrid(List<ProductModel> products, BuildContext context) {
+    return GridView.count(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        crossAxisCount: 2,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1.0,
+        childAspectRatio: 1 / 1.58,
+        children: List.generate(products.length,
+            (index) => buildGridProduct(products[index], context)));
+  }
+
+  Widget buildGridProduct(ProductModel model, context) => Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomStart,
+              children: [
+                Image(
+                  image: NetworkImage(model.image!),
+                  width: double.infinity,
+                  height: 200.0,
+                ),
+                if (model.discount != 0)
+                  Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 5.0,
+                    ),
+                    child: Text(
+                      'DISCOUNT',
+                      style: TextStyle(
+                        fontSize: 8.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    model.name!,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      height: 1.3,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        '${model.price.round()}',
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: defaultColor,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5.0,
+                      ),
+                      if (model.discount != 0)
+                        Text(
+                          '${model.oldPrice.round()}',
+                          style: TextStyle(
+                            fontSize: 10.0,
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        ),
+                      Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: CircleAvatar(
+                          radius: 15.0,
+                          backgroundColor: defaultColor,
+                          child: Icon(
+                            Icons.favorite_border,
+                            size: 14.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
 }
